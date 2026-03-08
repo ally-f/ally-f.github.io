5a05a0817ec8dd37b2cad122917163bf627e6935
ally fries <ally.fries@powerhrg.com>
2026-03-08 17:24:13 -0400

    Squashed commit of the following:
    commit bee2a0475670eca5108122d95042cb00c529c891
    Author: ally fries <ally.fries@powerhrg.com>
    Date:   Sun Mar 8 17:23:35 2026 -0400
    
        more menu button fixes

---
+++ src/components/home/home.ts
@@ -0,0 +1,122 @@
+// whether dark mode is enabled; default off 
+var dark = false;
+export default dark;
+
+
+/* Scroll to top button */
+export function scrollToTop() {
+  history.pushState({}, '', '#'); // changes the url to base url, with '#' appended (cannot pass a blank string...)
+  window.scrollTo(0, 0); // forces scroll to top 
+}
+
+
+/* dark mode, reference https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp */
+/*there has to be a way to optimize this; pull out the loop thing into its own method? */ 
+export function toggleDarkMode() {
+  document.body.classList.toggle("dark"); 
+
+  document.getElementById("menu")?.classList.toggle("dark");
+  document.getElementById("react-router-wrapper")?.classList.toggle("dark");
+
+  document.getElementById("stt")?.classList.toggle("dark");
+
+  document.getElementById("darkmode")?.classList.toggle("dark");
+
+  document.getElementById("content")?.classList.toggle("dark");
+
+  document.getElementById("header")?.classList.toggle("dark");
+
+  document.getElementById("footer")?.classList.toggle("dark");
+
+  let h1 = document.getElementsByTagName("h1");
+  for (let i = 0; i < h1.length; i++) {
+    h1[i].classList.toggle("dark");
+  }
+
+  let h2 = document.getElementsByTagName("h2");
+  for (let i = 0; i < h2.length; i++) {
+    h2[i].classList.toggle("dark");
+  }
+
+  let h3 = document.getElementsByTagName("h3"); 
+  for (let i = 0; i < h3.length; i++) {
+    h3[i].classList.toggle("dark");
+  }
+
+  let a = document.getElementsByTagName("a"); 
+  for (let i = 0; i < a.length; i++) {
+    a[i].classList.toggle("dark");
+  }
+
+  let skk = document.getElementsByClassName("skk"); 
+  for (let i = 0; i < skk.length; i++) {
+    skk[i].classList.toggle("dark");
+  }
+
+  let prns = document.getElementsByClassName("prns"); 
+  for (let i = 0; i < prns.length; i++) {
+    prns[i].classList.toggle("dark");
+  }
+
+  let mark = document.getElementsByClassName("mark"); 
+  for (let i = 0; i < mark.length; i++) {
+    mark[i].classList.toggle("dark");
+  }
+
+
+  let highlight = document.getElementsByClassName("highlight"); 
+  for (let i = 0; i < highlight.length; i++) {
+    highlight[i].classList.toggle("dark");
+  }
+
+  // toggle dark tracker
+  if (dark == false) {
+    dark = true;
+  }
+  else {
+    dark = false;
+  }
+}
+
+export function forceDark() {
+  if (dark) {
+    toggleDarkMode();
+    dark = false;
+  }
+}
+
+/* email address obfuscator */
+export function obfuscateEmail() {
+  let rra_liame = ['a', 'l', 'm', 'f', 'r', '0', '0', 'at', 'g', 'm', 'a', 'i', 'l', 'dot', 'c', 'o', 'm'];
+  // this format protects from web scrapers 
+
+  let rts_liame = '';
+  for (var i=0; i<rra_liame.length; i++) {
+    if (rra_liame[i] == "at") {
+      rts_liame = rts_liame + '@';
+    }
+    else if (rra_liame[i] == "dot") {
+      rts_liame = rts_liame + '.';
+    }
+    else {
+      rts_liame = rts_liame + rra_liame[i];
+    }
+  }
+  /*could create a new element, then insert that before or inside (- inside preferable, but how?) link element./*
+
+  /*could you also just return the plain text and embed it into the href directly...? */
+
+  return rts_liame;
+}
+
+/* opens the router navigation menu */
+export function expandMenu() {
+  let menu = document.getElementById("react-router-wrapper");
+  if (menu && menu.classList.contains("expanded")) {
+    menu.classList.remove("expanded");
+  }
+  else if (menu) {
+    menu.classList.add("expanded");
+  }
+}
+
